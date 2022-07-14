0d8111725f778a4bc6cebe8c9867d9ac5b2dffd0
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2022-07-14 17:10:14 +0100

    Menu fix

--- js/main.js
+++ js/main.js
@@ -1,32 +1,29 @@
 (function () {
   var setMenu = function () {
     // variables
-    var burgerMenuContainer = document.querySelector(".burgerMenuContainer");
-    if (window.getComputedStyle(burgerMenuContainer).display !== "none") {
-      var burgerMenu = document.querySelector(".burgerMenuContainer img");
-      var navBar = document.querySelector("nav");
-      var navBarStatus = false;
+    var burgerMenu = document.querySelector(".burgerMenuContainer img");
+    var navBar = document.querySelector("nav");
+    var navBarStatus = false;
+    navBar.removeAttribute("style");
+    // mobile menu
+    if (window.getComputedStyle(navBar).overflow == "hidden") {
+      var navHeight = navBar.offsetHeight;
+      navBar.style.height = "0px";
+      burgerMenu.addEventListener("click", function () {
+        if (navBarStatus) {
+          navBarStatus = false;
+          navBar.style.height = "0px";
+          burgerMenu.setAttribute("src", "images/whiteMenu.svg");
+        } else {
+          navBarStatus = true;
+          navBar.style.height = navHeight + "px";
+          burgerMenu.setAttribute("src", "images/whiteClose.svg");
+        }
+      });
+    } else {
+      navBarStatus = false;
       navBar.removeAttribute("style");
-      // mobile menu
-      if (window.getComputedStyle(navBar).overflow == "hidden") {
-        var navHeight = navBar.offsetHeight;
-        navBar.style.height = "0px";
-        burgerMenu.addEventListener("click", function () {
-          if (navBarStatus) {
-            navBarStatus = false;
-            navBar.style.height = "0px";
-            burgerMenu.setAttribute("src", "images/whiteMenu.svg");
-          } else {
-            navBarStatus = true;
-            navBar.style.height = navHeight + "px";
-            burgerMenu.setAttribute("src", "images/whiteClose.svg");
-          }
-        });
-      } else {
-        navBarStatus = false;
-        navBar.removeAttribute("style");
-        burgerMenu.setAttribute("src", "images/whiteMenu.svg");
-      }
+      burgerMenu.setAttribute("src", "images/whiteMenu.svg");
     }
   };
   setMenu();
